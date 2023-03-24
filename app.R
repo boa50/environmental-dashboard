748273e7a828abce9039e18ed5615498a506fe26
boa50 <bruno.albuquerque50@gmail.com>
2023-03-24 17:16:38 +1030

    Added a loading animation before loading the app

--- app.R
+++ app.R
@@ -5,25 +5,31 @@ library(plotly)
 library(janitor)
 library(leaflet)
 library(maps)
+library(shinycssloaders)
 
 theme_set(theme_minimalistic())
 
 ui <- fluidPage(
-    titlePanel("Envronmental Dashboard"),
-    fluidRow(
-      column(3, 
-             selectInput("selected_country", 
-                         "Country",
-                         c(all_countries, unique(df$country))))
-    ),
-    fluidRow(
-      column(6, linePlotUI("line_plot")),
-      column(6, mapPlotUI("map_plot"))
-    )
+  pageSpinner(type = 1, color = "#0dc5c1"),
+  titlePanel("Envronmental Dashboard"),
+  fluidRow(
+    column(3, 
+           selectInput("selected_country", 
+                       "Country",
+                       c(all_countries, unique(df$country))))
+  ),
+  fluidRow(
+    column(6, linePlotUI("line_plot")),
+    column(6, mapPlotUI("map_plot"))
+  )
 )
 
 server <- function(input, output, session) {
+  showPageSpinner()
   linePlotServer("line_plot", reactive(input$selected_country))
+  
+  highlighted_country <- mapPlotServer("map_plot",
+                                       reactive(input$selected_country))
   
   highlighted_country <- mapPlotServer("map_plot",
                                        reactive(input$selected_country))
@@ -32,6 +38,8 @@ server <- function(input, output, session) {
     updateSelectInput(session,
                       "selected_country",
                       selected = highlighted_country())
+    
+    hidePageSpinner()
   })
 }
 
